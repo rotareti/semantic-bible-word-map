f8e9eeb1514cb00587ffa553a713578a6ba7f8fd
rotareti <225734273+rotareti@users.noreply.github.com>
2026-08-13 17:03:42 -0600

    Fix: remove zoom ceiling completely, increase auto-label threshold to 250

--- web/bible-word-map.js
+++ web/bible-word-map.js
@@ -287,7 +287,7 @@ class BibleWordMap extends HTMLElement {
         new ResizeObserver(() => this.resize()).observe(this.canvas.parentElement);
         
         this.zoom = d3.zoom()
-            .scaleExtent([0.05, 500])
+            .scaleExtent([0.05, 100000])
             .on("zoom", (e) => {
                 this.transform = e.transform;
                 this.draw();
@@ -629,7 +629,7 @@ class BibleWordMap extends HTMLElement {
                 visibleNodesCount++;
             }
         });
-        let autoShowLabels = visibleNodesCount < 150;
+        let autoShowLabels = visibleNodesCount < 250;
         
         
         this.ctx.save();
