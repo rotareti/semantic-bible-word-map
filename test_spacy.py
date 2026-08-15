07e182fe2c4e2cf846672a1e7cc6b0e919195936
rotareti <225734273+rotareti@users.noreply.github.com>
2026-08-14 18:59:53 -0600

    UI: Add z-index to header so dropdowns render above the canvas

--- test_spacy.py
+++ test_spacy.py
@@ -1,5 +1,5 @@
 import spacy
 nlp = spacy.load("en_core_web_sm", disable=["parser", "ner"])
-doc = nlp("I'm sure he won't do it because they can't.")
+doc = nlp("I don't know")
 for token in doc:
     print(f"{token.text} -> {token.lemma_} ({token.pos_})")
