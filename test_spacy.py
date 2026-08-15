6d07797b51c393e76e32f1f5995ad6e487822961
rotareti <225734273+rotareti@users.noreply.github.com>
2026-08-14 19:08:48 -0600

    Cleanup test script

--- test_spacy.py
+++
@@ -1,5 +0,0 @@
-import spacy
-nlp = spacy.load("en_core_web_sm", disable=["parser", "ner"])
-doc = nlp("I don't know")
-for token in doc:
-    print(f"{token.text} -> {token.lemma_} ({token.pos_})")
